91b27ccfa40d4fed4f2f7b25cfaa2db2e1ffe0ee
ilkayGkbdk <ilkayrg55@hotmail.com>
2024-06-04 18:32:18 +0300

    background

---
+++ objects/background.py
@@ -0,0 +1,11 @@
+import pygame
+
+import assets
+
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        self.image = assets.get_sprites("background")
+        self.rect = self.image.get_rect(topleft=(0, 0))
+
+        super().__init__(*groups)
